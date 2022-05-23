5c38d163c086f74057838bb84a3e6c845aca32db
Ramazan Salakhov <64357780+sal-ram@users.noreply.github.com>
2022-05-23 20:20:06 +0300

    added bullet impacts on objects

--- Assets/Scripts/SingleShotGun.cs
+++ Assets/Scripts/SingleShotGun.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class SingleShotGun : Gun
 {
     [SerializeField] Camera cam;
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
 
     public override void Use()
     {
@@ -18,6 +25,20 @@ public class SingleShotGun : Gun
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+            PV.RPC("RPC_Shoot", RpcTarget.All, hit.point, hit.normal);
         }
     }
+
+    [PunRPC]
+    void RPC_Shoot(Vector3 hitPosition, Vector3 hitNormal)
+    {
+        Collider[] colliders = Physics.OverlapSphere(hitPosition, 0.2f);
+
+        if (colliders.Length != 0)
+        {
+            GameObject bulletImp =  Instantiate(bulletImpact, hitPosition, Quaternion.LookRotation(hitNormal));
+            bulletImp.transform.SetParent(colliders[0].transform);
+        }
+    }
+
 }
